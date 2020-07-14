628adca62e1bf88418eacda79ab17dc026a070a7
baldgold <alexmulkidzhanov@gmail.com>
2020-07-14 20:24:19 +0300

    Work with wx and Dialog window

--- resize_picture.py
+++ resize_picture.py
@@ -1,24 +1,35 @@
 import cv2
+import wx.lib.imagebrowser as imagebrowser
+import wx
 
-image_address = input('Введите адрес изображения, которое желаете масштабировать в 2 раза:\n>>>')
 
-src = cv2.imread(image_address, cv2.IMREAD_UNCHANGED)
+if __name__ == "__main__":
 
-if src is None:
-    print('Изображение не найдено!')
-else:
-    # процент, на который измениться размер изображения
-    scale_percent = 200
+    app = wx.App(False)
+    dialog = imagebrowser.ImageDialog(None)
+    if dialog.ShowModal() == wx.ID_OK:
+        image_address = dialog.GetFile()
+        print("Вы выбрали файл: " + image_address)
+    dialog.Destroy()
 
-    # расчитываем 200% от исходного размера
-    width = int(src.shape[1] * scale_percent / 100)
-    height = int(src.shape[0] * scale_percent / 100)
+    src = cv2.imread(image_address, cv2.IMREAD_UNCHANGED)
 
-    # устанавливаем желаемый размер для изображения
-    new_size = (width, height)
+    if src is None:
+        wx.MessageBox('Вы выбрали НЕ изображение\nПопробуйте ещё раз.', 'Проблема с изображением', wx.OK | wx.ICON_WARNING)
+        print('Изображение не найдено!')
+    else:
+        # процент, на который измениться размер изображения
+        scale_percent = 200
 
-    output = cv2.resize(src, new_size)
+        # расчитываем 200% от исходного размера
+        width = int(src.shape[1] * scale_percent / 100)
+        height = int(src.shape[0] * scale_percent / 100)
 
-    # записываем вывод cv2.resize в локальный файл изображения
-    cv2.imwrite('new_image_2x.png', output)
-    print('Дело сделано!')
+        # устанавливаем желаемый размер для изображения
+        new_size = (width, height)
+
+        output = cv2.resize(src, new_size)
+
+        # записываем вывод cv2.resize в локальный файл изображения
+        cv2.imwrite('new_image_x2.png', output)
+        print('Дело сделано!')
